e33f44d82b286504676652a177218e600b99b6db
areefhida <areef.hiide@gmail.com>
2017-12-13 11:00:36 +0700

    add menu user management

--- resources/views/layouts/partials/sidenav.blade.php
+++ resources/views/layouts/partials/sidenav.blade.php
@@ -3,5 +3,6 @@
         <li class="nav-item"><a class="nav-link" href="{{ route('category') }}">Category</a></li>
         <li class="nav-item"><a class="nav-link" href="{{ route('subcategory') }}">Sub Category</a></li>
         <li class="nav-item"><a class="nav-link" href="{{ route('product.index') }}">Product</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('usermanagement.index') }}">User Management</a></li>
     </ul>
 </nav>
